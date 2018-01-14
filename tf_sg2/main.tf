382b9732bb1491c5b318461ad0318c55325b4dff
Cesar Rodriguez <cesar_rodriguez@troweprice.com>
2018-01-13 22:46:32 -0500

    modified file in subdir

--- tf_sg2/main.tf
+++ tf_sg2/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "sg" {
     from_port   = "22"
     to_port     = "22"
     protocol    = "tcp"
-    cidr_blocks = ["172.31.0.0/16"]
+    cidr_blocks = ["172.31.0.1/32"]
   }
 }
 
