c7ca124144ac446b1e6aa35ec68018c9866a55d2
Cesar Rodriguez <cesar_rodriguez@troweprice.com>
2018-01-13 21:25:27 -0500

    fixes all errors

--- main.tf
+++ main.tf
@@ -6,13 +6,10 @@ resource "aws_security_group" "sg" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    from_port = "22"
-    to_port   = "22"
-    protocol  = "tcp"
-
-    # cidr_blocks = ["172.31.0.0/16"]
-    
-    cidr_blocks_typo = ["0.0.0.0/0"]
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
   }
 }
 
